e6359652fd34dd557e20fd6d4fe5a88a77bfa484
cmptbeck <T.Beck@2019.ljmu.ac.uk>
2022-11-23 11:17:15 +0000

    Basic Functionality Added

--- Program.cs
+++ Program.cs
@@ -6,7 +6,24 @@ namespace DecimalToBinary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int input = 3684;
+
+            string binary = decimalToBinary(input);
+
+            Console.WriteLine(input + " converted to Binary is: " + binary);
+        }
+
+        static string decimalToBinary(int input) 
+        {   
+            string binary = "";
+
+            while (input > 0) 
+            {
+                binary = (input % 2) + binary;
+                input = ((input - (input % 2)) / 2);
+            }
+
+            return binary;
         }
     }
 }
