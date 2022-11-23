d5c53d4e6ff8dd2b14fb256295eb3eba1bea7a6d
cmptbeck <T.Beck@2019.ljmu.ac.uk>
2022-11-23 11:08:05 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DecimalToBinary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
